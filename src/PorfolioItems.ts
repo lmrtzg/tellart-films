a2ceae580aca040e83d6ea380481f047136c308a
Gabriel Lumertz <61801407+lmrtzg@users.noreply.github.com>
2021-04-13 18:05:11 -0300

    Atualizado portfólio

--- src/PorfolioItems.ts
+++ src/PorfolioItems.ts
@@ -9,76 +9,55 @@ export interface IPortfolioItems {
 const PortfolioItems: Array<IPortfolioItems> = [
   {
     name: 'port-1',
-    company: 'Freesurf',
-    job: 'Um Princípio de liberdade',
+    company: 'Draken',
+    job: 'Arte em Mandeira',
     link:
-      'https://player.vimeo.com/video/443425341?title=0&byline=0&portrait=0',
+      'https://player.vimeo.com/video/534119407?title=0&byline=0&portrait=0',
   },
   {
     name: 'port-2',
+    company: 'Marge',
+    job: 'Estagnação',
+    link:
+      'https://player.vimeo.com/video/534124829?title=0&byline=0&portrait=0',
+  },
+  {
+    name: 'port-3',
     company: 'Júnior Damiani',
     job: 'A Vida em Movimento',
     link:
       'https://player.vimeo.com/video/375627926?title=0&byline=0&portrait=0',
   },
   {
-    name: 'port-3',
+    name: 'port-4',
     company: 'Jony',
     job: 'Videoclipe',
     link:
       'https://player.vimeo.com/video/443425008?title=0&byline=0&portrait=0',
   },
   {
-    name: 'port-4',
-    company: 'Ahlma',
-    job: 'Série Comercial - Ep.1',
-    link:
-      'https://player.vimeo.com/video/306053726?title=0&byline=0&portrait=0',
-  },
-  {
     name: 'port-5',
-    company: 'Ahlma',
-    job: 'Série Comercial - Ep.2',
-    link:
-      'https://player.vimeo.com/video/306054048?title=0&byline=0&portrait=0',
-  },
-  {
-    name: 'port-6',
-    company: 'Ahlma',
-    job: 'Série Comercial - Ep.3',
-    link:
-      'https://player.vimeo.com/video/325870708?title=0&byline=0&portrait=0',
-  },
-  {
-    name: 'port-7',
-    company: 'Freesurf',
-    job: 'Lançamento Freesurf Mooving',
-    link:
-      'https://player.vimeo.com/video/272860752?title=0&byline=0&portrait=0',
-  },
-  {
-    name: 'port-8',
     company: 'TDK',
     job: 'CAP Operating System',
     link:
       'https://player.vimeo.com/video/394441015?title=0&byline=0&portrait=0',
   },
   {
-    name: 'port-9',
+    name: 'port-6',
     company: 'MARGE',
     job: 'Punk do Otário',
     link:
       'https://player.vimeo.com/video/474005250?title=0&byline=0&portrait=0',
   },
   {
-    name: 'port-10',
+    name: 'port-7',
     company: 'Sabores da Adri',
     job: 'Conheça os Sabores da Adri',
     link:
       'https://player.vimeo.com/video/508486454?title=0&byline=0&portrait=0',
   },
   {
-    name: 'port-11',
+    name: 'port-8',
     company: 'Owell',
     job: 'You Make Your Own Swell',
     link:
